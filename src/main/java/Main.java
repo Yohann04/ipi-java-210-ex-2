df0ba22ff0b8ba05fbde63f1bfe4f5431b72fc48
Yohann <yohann.bernard@ipilyon.net>
2022-01-12 16:53:22 +0100

    Exercice 6

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -16,6 +16,9 @@ public class Main {
 
     public static void main(String[] args) {
         initPersonnage();
+        short ennemi = 5;
+        ennemi = attaqueJoueur(ennemi);
+        System.out.println("il reste " + ennemi + " points de vie à l'ennemi");
     }
 
     public static void initPersonnage() {
@@ -29,7 +32,7 @@ public class Main {
         //Affecter la variable ptsDeVie
         ptsDeVie = MAX_PTS_VIE;
         //Affecter la variable ptsBouclier
-        ptsBouclier = PTS_BOUCLIER;
+        ptsBouclier = bouclierActif ? PTS_BOUCLIER : 0;
         scanner.close();
     }
 
@@ -40,13 +43,23 @@ public class Main {
 
     }
 
-    public static short nombreAuHasard(short nombre){
-        return  (short) Math.round(Math.random() * nombre);
+    public static short nombreAuHasard(short nombre) {
+        return (short) Math.round(Math.random() * nombre);
 
     }
 
-    public static short attaqueJoueur(short MAX_VIE_ENNEMI){
-        MAX_VIE_ENNEMI
+    public static short attaqueJoueur(short ptsVieEnnemi) {
+        //Déterminer la force de l'attaque du joueur
+        short forceAttaque = nombreAuHasard(MAX_ATTAQUE_JOUEUR);
+        //Retrancher les points de l'attaque sur les points de vie de l'ennemi
+        ptsVieEnnemi -= forceAttaque;
+        //Afficher les caractéristiques de l'attaque
+        System.out.println(Util.color(nomPersonnage, Color.GREEN)
+                + " attatque l'" + Util.color("ennemi", Color.YELLOW) + "! Il lui fait perde "
+                + Util.color(forceAttaque, Color.PURPLE) + " points de dommages");
+        //Retourner le nombre de points de vie de l'ennemi
+        return ptsVieEnnemi;
     }
 
 }
+
