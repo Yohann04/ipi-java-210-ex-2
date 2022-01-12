a1a9c2bc455afadab02bb7e429e82bf5dee17540
Yohann <yohann.bernard@ipilyon.net>
2022-01-12 12:57:52 +0100

    Exercice 1

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -8,4 +8,30 @@ public class Main {
     public static final short MAX_ATTAQUE_JOUEUR = 5;
     public static final short REGENARATION_BOUCLIER_PAR_TOUR = 10;
 
+    public static String nomPersonnage;
+    public static short ptsDeVie;
+    public static short ptsBouclier;
+    public static short nbEnnemisTues;
+    public static boolean bouclierActif = true;
+
+    public static void main(String[] args) {
+        initPersonnage();
+    }
+
+    public static void initPersonnage(){
+        //Afficher le message de saisi
+        System.out.println("Saisir le nom de votre personnage");
+        //Lire la saisie utilisateur dans ma variable nomPersonnage
+         Scanner scanner = new Scanner(System.in);
+         nomPersonnage = scanner.nextLine();
+         //Afficher le message cest partit !
+        System.out.println("OK " + Util.color(nomPersonnage, Color.GREEN) + " !" + " C'est parti !");
+        //Affecter la variable ptsDeVie
+        ptsDeVie = MAX_PTS_VIE;
+        //Affecter la variable ptsBouclier
+        ptsBouclier = PTS_BOUCLIER;
+        scanner.close();
+    }
+
+
 }
